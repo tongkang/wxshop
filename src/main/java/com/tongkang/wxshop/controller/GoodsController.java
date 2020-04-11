20e14d95bb5a99ea2221bb904a1c15169963ee40
tongkang <tongkang97@gmail.com>
2020-04-11 18:00:19 +0800

    增加商品的查找和更新

--- src/main/java/com/tongkang/wxshop/controller/GoodsController.java
+++ src/main/java/com/tongkang/wxshop/controller/GoodsController.java
@@ -1,15 +1,20 @@
 package com.tongkang.wxshop.controller;
 
-import com.tongkang.wxshop.dao.GoodsDao;
+import com.tongkang.wxshop.entity.HttpException;
+import com.tongkang.wxshop.entity.PageResponse;
 import com.tongkang.wxshop.entity.Response;
 import com.tongkang.wxshop.generator.Goods;
 import com.tongkang.wxshop.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -46,13 +51,35 @@ public class GoodsController {
         try {
             response.setStatus(HttpServletResponse.SC_NO_CONTENT);
             return Response.of(goodsService.deleteGoodsById(goodsId));
-        } catch (GoodsService.NotAuthorizedForShopException e) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return Response.of(e.getMessage(), null);
-        } catch (GoodsDao.ResourceNotFoundException e) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (HttpException e) {
+            response.setStatus(e.getStatusCode());
             return Response.of(e.getMessage(), null);
         }
+    }
+
+    //更新商品
+    @PatchMapping("/goods/{id}")
+    public Response<Goods> updateGoods(@PathVariable("id") Long id,
+                                       @RequestBody Goods goods,
+                                       HttpServletResponse response) {
+        clean(goods);
+        goods.setId(id);
+        try {
+            return Response.of(goodsService.updateGoods(goods));
+        } catch (HttpException e) {
+            response.setStatus(e.getStatusCode());
+            return Response.of(e.getMessage(), null);
+        }
+
+    }
+
+    //查询所有商品
+    @GetMapping("/goods")
+    public @ResponseBody
+    PageResponse<Goods> getGoods(@RequestParam("pageNum") Integer pageNum,
+                                 @RequestParam("pageSize") Integer pageSize,
+                                 @RequestParam(value = "shopId", required = false) Integer shopId) {
+        return goodsService.getGoods(pageNum, pageSize, shopId);
     }
 
     private void clean(Goods goods) {
--- src/main/java/com/tongkang/wxshop/service/GoodsService.java
+++ src/main/java/com/tongkang/wxshop/service/GoodsService.java
@@ -1,50 +1,110 @@
 package com.tongkang.wxshop.service;
 
-import com.tongkang.wxshop.dao.GoodsDao;
-import com.tongkang.wxshop.dao.ShopDao;
+import com.tongkang.wxshop.entity.DataStatus;
+import com.tongkang.wxshop.entity.HttpException;
+import com.tongkang.wxshop.entity.PageResponse;
 import com.tongkang.wxshop.generator.Goods;
+import com.tongkang.wxshop.generator.GoodsExample;
+import com.tongkang.wxshop.generator.GoodsMapper;
 import com.tongkang.wxshop.generator.Shop;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.tongkang.wxshop.generator.ShopMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
 public class GoodsService {
 
-    private final GoodsDao goodsDao;
-    private final ShopDao shopDao;
+    private GoodsMapper goodsMapper;
+    private ShopMapper shopMapper;
 
-    @Autowired
-    public GoodsService(GoodsDao goodsDao, ShopDao shopDao) {
-        this.goodsDao = goodsDao;
-        this.shopDao = shopDao;
+    public GoodsService(GoodsMapper goodsMapper, ShopMapper shopMapper) {
+        this.goodsMapper = goodsMapper;
+        this.shopMapper = shopMapper;
     }
+
 
     public Goods createGoods(Goods goods) {
 
 //        return goodsDao.insertGoods(goods);
 
 //        要在所在的商店中才能进行创建商品
-        Shop shop = shopDao.findShopById(goods.getShopId());
-
-        if (Objects.equals(shop.getOwnerUserId(), UserContext.getCurrentUser().getId())) {
-            return goodsDao.insertGoods(goods);
-        } else {
-            throw new NotAuthorizedForShopException("无权访问！");
-        }
+//        Shop shop = shopMapper.selectByPrimaryKey(goods.getShopId());
+//
+//        if (Objects.equals(shop.getOwnerUserId(), UserContext.getCurrentUser().getId())) {
+        goods.setStatus(DataStatus.OK.getName());
+        long id = goodsMapper.insert(goods);
+        goods.setId(id);
+        return goods;
+//        } else {
+//            throw HttpException.forbidden("无权访问！");
+//        }
     }
 
     public Goods deleteGoodsById(Long goodsId) {
-//        return goodsDao.deleteGoodsById(goodsId);
+        Shop shop = shopMapper.selectByPrimaryKey(goodsId);
 
-        Shop shop = shopDao.findShopById(goodsId);
         if (Objects.equals(shop.getOwnerUserId(), UserContext.getCurrentUser().getId())) {
-            return goodsDao.deleteGoodsById(goodsId);
+            Goods goods = goodsMapper.selectByPrimaryKey(goodsId);
+            if (goods == null) {
+                throw HttpException.notFound("未找到！");
+            }
+            goods.setStatus(DataStatus.DELETED.getName());
+            goodsMapper.updateByPrimaryKey(goods);
+
+            return goods;
         } else {
-            throw new NotAuthorizedForShopException("无权访问！");
+            throw HttpException.forbidden("无权访问！");
         }
 
+    }
+
+    public PageResponse<Goods> getGoods(Integer pageNum, Integer pageSize, Integer shopId) {
+        //知道有多少个元素
+        //然后才知道有多少页
+        //然后才能正确的分页
+        int totalNumber = countGoods(shopId);
+        int totalPage = totalNumber % pageSize == 0 ? totalNumber / pageSize : totalNumber / pageSize + 1;
+
+        GoodsExample page = new GoodsExample();
+        page.setLimit(pageSize);
+        page.setOffset((pageNum - 1) * pageSize);
+
+        List<Goods> pagedGoods = goodsMapper.selectByExample(page);
+
+        return PageResponse.pagedData(pageNum, pageSize, totalPage, pagedGoods);
+    }
+
+    private int countGoods(Integer shopId) {
+        if (shopId == null) {
+            GoodsExample goodsExample = new GoodsExample();
+            goodsExample.createCriteria()
+                    .andStatusEqualTo(DataStatus.OK.getName());
+            return (int) goodsMapper.countByExample(goodsExample);
+        } else {
+            GoodsExample goodsExample = new GoodsExample();
+            goodsExample.createCriteria()
+                    .andStatusEqualTo(DataStatus.OK.getName())
+                    .andShopIdEqualTo(shopId.longValue());
+            return (int) goodsMapper.countByExample(goodsExample);
+        }
+    }
+
+    public Goods updateGoods(Goods goods) {
+//        Shop shop = shopMapper.selectByPrimaryKey(goods.getShopId());
+//
+//        if (Objects.equals(shop.getOwnerUserId(), UserContext.getCurrentUser().getId())) {
+            GoodsExample byId = new GoodsExample();
+            byId.createCriteria().andIdEqualTo(goods.getId());
+            int affecteRows = goodsMapper.updateByExample(goods, byId);
+            if (affecteRows == 0) {
+                throw HttpException.notFound("没找到");
+            }
+            return goods;
+//        } else {
+//            throw HttpException.forbidden("无权访问！");
+//        }
     }
 
     public static class NotAuthorizedForShopException extends RuntimeException {
